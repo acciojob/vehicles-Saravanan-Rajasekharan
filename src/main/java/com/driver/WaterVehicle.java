76449cd5b7d599923bf976c959bcab74a337a092
Saravanan Rajasekharan <saravanan.rajasekharan@gmail.com>
2023-01-21 15:16:52 +0530

    solution3

--- src/main/java/com/driver/WaterVehicle.java
+++ src/main/java/com/driver/WaterVehicle.java
@@ -7,19 +7,4 @@ public interface WaterVehicle {
 
 }
 
-class Boat implements WaterVehicle{
-    private String name;
 
-    private int capacity;
-
-
-    @Override
-    public String getVehicleName() {
-        return name;
-    }
-
-    @Override
-    public int getVehicleCapacity() {
-        return capacity;
-    }
-}
